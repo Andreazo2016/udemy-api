5ce8dc254266f30f33afc1dc570d6824eb6c56a5
Andreazo Silva <andreazocoisas@gmail.com>
2024-08-18 11:13:21 -0300

    feat: add logger mapping and implement

--- src/controllers/course.js
+++ src/controllers/course.js
@@ -2,6 +2,7 @@ const HttpStatus = require('http-status')
 const UdemyService = require('../services/udemy')
 const formatCourse = require('../helpers/format_course')
 const { db } = require('../config/db')
+const Logger = require('../helpers/logger')
 
 
 class CourseController {
@@ -9,7 +10,13 @@ class CourseController {
   async create(req, res) {
     try {
       const { course_id } = req.body
-      
+      Logger.tracking({
+        event_name: 'NewCreateCourseNotification',
+        type_event: 'CreateCourse',
+        body: {
+          course_id
+        }
+      })
       const [result] = await db('udemy_courses').select('id').where({
         course_id
       })
@@ -21,8 +28,21 @@ class CourseController {
       const response = await UdemyService.getCourse(course_id)
       const course = formatCourse(response)
       await db('udemy_courses').insert([course])
+      Logger.tracking({
+        event_name: 'CreatedCourse',
+        type_event: 'CreateCourse',
+        method: 'CourseController.create',
+        body: {
+          course
+        }
+      })
       return res.status(HttpStatus.CREATED).send()
     } catch (ex) {
+      Logger.error({
+        error: ex,
+        method: 'CourseController.create',
+        type_event:'CreateCourse'
+      })
       return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({
         message: 'Internal Server Error'
       })
